d268e5e52ca8eb8a11373d336888abc6e16a2fa0
danicode <danielanzawa@gmail.com>
2023-10-10 21:14:07 -0300

    Add api-rest

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,9 @@ Encore
     // uncomment if you use React
     //.enableReactPreset()
 
+    // Enable Vue loader
+    .enableVueLoader()
+
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
